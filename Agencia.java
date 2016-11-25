536151e56b8c6b8a2215a3ae1501a629a406b2ab
Arie_Azzi <ariel_azzi@hotmail.com>
2016-11-24 22:06:22 -0200

    Inseri o metodo menuDeTransacoes, ja com a opção 1, onde a mesma abre uma conta

--- Agencia.java
+++ Agencia.java
@@ -9,7 +9,7 @@ public class Agencia
         qtd = 0;
     }
 
-    public int abreConta()
+    private int abreConta()
     {
         if (poupancas.length == qtd)
             return -1;
@@ -48,12 +48,131 @@ public class Agencia
         }
     }
 
-    public int buscarConta(int numConta)
+    private int buscarConta(int numConta)
     {
         int aux = -1; 
         for (int i=0;i < poupancas.length; i++)
-            if (poupancas[i].getNumero() == numConta)
-                aux = i;
+            if(poupancas[i] != null)
+                if (poupancas[i].getNumero() == numConta)
+                    aux = i;
         return aux;
     }
+
+    public void menuDeTransacoes()
+    {
+        Teclado t = new Teclado();
+        int op;
+        do
+        {
+            System.out.println("\f1 - Abre Conta");
+            System.out.println("2 - Deposita");
+            System.out.println("3 - Retira");
+            System.out.println("4 - Retira Para Saúde");
+            System.out.println("5 - Amortiza financiamento");
+            System.out.println("6 - Emite extrato da conta");
+            System.out.println("7 - Credita rendimento");
+            System.out.println("8 - Insere um dependente");
+            System.out.println("9 - Retira um dependente");
+            System.out.println("10 - Encerra");
+            
+            do{
+                op = t.leInt("Selecione a opção do menu: ");
+                if(op<1 || op>10)
+                    System.out.println("Opção invalida, Digite novamente!");
+            }while(op<1 || op>10);
+
+            if(op == 1)
+            {
+                int result = abreConta();
+                if (result == -1)
+                    System.out.println("Não pode abrir novas contas nesta agência");
+                else
+                    System.out.println("Conta aberta de numero: " + result);
+            }
+            // else if(op == 2)
+            // {
+            //     int indice = buscarConta(t.leInt("Informe o numero da conta:"));
+            //     if (indice == -1)
+            //         System.out.println("Conta Inexistente");
+            //     else
+            //         poupancas[indice].deposita(t.leDouble("Informe o valor do deposito"));
+            // }
+            // else if(op == 3)
+            // {
+            //     int indice = buscarConta(t.leInt("Informe o numero da conta:"));
+            //     if (indice == -1)
+            //         System.out.println("Conta Inexistente");
+            //     else{
+            //         boolean ret = poupancas[indice].retira(t.leDouble("Informe o valor da retirada"));
+            //         if (!ret)
+            //             System.out.println("Saldo Insuficiente");
+            //     }
+
+            // }
+            // else if(op == 4)
+            //     seq.geraExibeMultiplosCalcMed(t.leInt("Digite um numero: "));
+            // else if(op == 5)
+            //     seq.geraExibeDivisores();
+            // else if(op == 6)
+            //     seq.geraExibePrimos();
+            // else if(op == 7)
+            //     seq.geraExibePrimos();
+            // else if(op == 8)
+            //     seq.geraExibePrimos();
+            // else if(op == 9)
+            //     seq.geraExibePrimos();
+
+            // op = t.leInt("Informe uma opção:");
+            if(op != 10)
+                t.leInt("Tecle Enter para exibir o menu novamente");
+
+
+        }while(op != 10);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // do{
+ //            System.out.println("\f------------------------------");
+ //            System.out.println("1 – Gera e exibe inteiros");
+ //            System.out.println("2 – Gera e exibe Pares");
+ //            System.out.println("3 – Gera e exibe  múltiplos comuns de 3 e 5");
+ //            System.out.println("4 – Exibe a média aritmética dos múltiplos");
+ //            System.out.println("5 – Gera e exibe divisores");
+ //            System.out.println("6 – Gera e exibe Primos");
+ //            System.out.println("7 – Sair");
+ //            System.out.println("------------------------------");
+            
+ //            do{
+ //                op = t.leInt("Selecione a opção do menu: ");
+ //                if(op<1 || op>7)
+ //                    System.out.println("Opção invalida, Digite novamente!");
+ //            }while(op<1 || op>7);
+
+ //            if(op == 1)
+ //                seq.geraExibeInt();
+ //            else if(op == 2)
+ //                seq.geraExibePares();
+ //            else if(op == 3)
+ //                seq.geraExibeMultiplos();
+ //            else if(op == 4)
+ //                seq.geraExibeMultiplosCalcMed(t.leInt("Digite um numero: "));
+ //            else if(op == 5)
+ //                seq.geraExibeDivisores();
+ //            else if(op == 6)
+ //                seq.geraExibePrimos();
+            
+ //            if(op != 7)
+ //                t.leInt("Tecle Enter para exibir o menu novamente");
+ //        }while(op != 7);
